dea52fbdbe7fe244c4d5f2f069a074c1382c24a2
name <mateuszkdwww@gmail.com>
2022-08-23 15:15:25 -0300

    Entrega feita

--- katas2.js
+++ katas2.js
@@ -1,37 +1,44 @@
-// comece a criar a sua função add na linha abaixo
+function add(numb_1, numb_2){
+    return numb_1 + numb_2
+}
 
+console.log(add(8,8))
+console.assert(add(3, 5) === 8, 'A função add não está funcionando como esperado');
 
-// descomente a linha seguinte para testar sua função
-// console.assert(add(3, 5) === 8, 'A função add não está funcionando como esperado');
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+function multiply(mult_numb_1, mult_numb_2){
+        let multi = 0
+        for(let i = 0; i < mult_numb_2; i++){
+            multi = (add(multi, mult_numb_1))
+        }
+        return multi
+}
 
-// comece a criar a sua função multiply na linha abaixo
+console.log(multiply(5, 5))
+console.assert(multiply(4, 6) === 24, 'A função multiply não está funcionando como esperado');
 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// descomente a linha seguinte para testar sua função
-// console.assert(multiply(4, 6) === 24, 'A função multiply não está funcionando como esperado');
+function power(x, n){
+    let expo = 1
+    for(let i = 0; i < n; i++){
+        expo = multiply(expo, x)
+    }
+    return expo
+}
 
+console.log(power(2, 8))
+console.assert(power(3, 4) === 81, 'A função power não está funcionando como esperado');
 
-// comece a criar a sua função power na linha abaixo
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-
-// descomente a linha seguinte para testar sua função
-// console.assert(power(3, 4) === 81, 'A função power não está funcionando como esperado');
-
-
-// comece a criar a sua função factorial na linha abaixo
-
-
-// descomente a linha seguinte para testar sua função
-// console.assert(factorial(5) === 120, 'A função factorial não está funcionando como esperado');
-
-
-/**
- * BONUS (aviso: o grau de dificuldade é bem maior !!!)
- */
-
-// crie a função fibonacci
-
-
-// descomente a linha seguinte para testar sua função
-// console.assert(fibonacci(7) === 13, 'A função fibonacci não está funcionando como esperado');
+function factorial(numb){
+    let result = 1
+    for(let i = 1; i <= numb; i++){
+          result = multiply(result, i)
+    }
+    return result
+}
+console.log(factorial(6))
+console.assert(factorial(5) === 120, 'A função factorial não está funcionando como esperado');
